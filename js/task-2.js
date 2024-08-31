e5caeaa86548dd3f5d09a57ed35600def6321d34
Daria <dariaasieieva@gmail.com>
2024-08-31 23:46:30 +0200

    1

--- js/task-2.js
+++ js/task-2.js
@@ -3,7 +3,7 @@
 
 
 function formatMessage(message, maxLength) {
-    if (message <= maxLength) {
+    if (message.length <= maxLength) {
         return message;
     } else {
         return message.slice(0, maxLength) + "...";
